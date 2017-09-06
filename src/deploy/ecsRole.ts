38db315c0dafc64d7d27a6e722884ee86dbd494d
Borza Viktor <viktor.borza@jaydata.org>
2017-09-06 16:19:26 +0200

    ADD: ECS run task policy

---
+++ src/deploy/ecsRole.ts
@@ -0,0 +1,43 @@
+export default ({ logger }) => {
+    return {
+        hooks: {
+            "after:IAM-Role-corp-check-rest": async (context) => {
+                try {
+                    const roleResource = context.CloudFormationTemplate.Resources.IAMcorpcheckrest
+                    const policies = roleResource.Properties.Policies
+                    policies.push({
+                        "PolicyName": {
+                            "Fn::Join": [
+                                "-",
+                                [
+                                    "functionly",
+                                    "corp-check-rest",
+                                    "ecs"
+                                ]
+                            ]
+                        },
+                        "PolicyDocument": {
+                            "Version": "2012-10-17",
+                            "Statement": [
+                                {
+                                    "Effect": "Allow",
+                                    "Action": [
+                                        "ecs:RunTask"
+                                    ],
+                                    "Resource": [
+                                        {
+                                            "Fn::Sub": "arn:aws:ecs:${AWS::Region}:${AWS::AccountId}:*"
+                                        }
+
+                                    ]
+                                }
+                            ]
+                        }
+                    })
+                } catch (e) { 
+                    logger.error(`ERROR when add ECS role: ${e.message}`)
+                }
+            }
+        }
+    }
+}
