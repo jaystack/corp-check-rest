53f48c3b5193b9b6ec875279702e64bfbd46abc7
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-09-20 18:02:55 +0200

    type enhancement

--- src/utils/reduceTree.ts
+++ src/utils/reduceTree.ts
@@ -4,12 +4,12 @@ export type Reducer<R> = {
   (acc: R, pkg: Package, path: string[]): R;
 };
 
-const reduce = <R>(reducer: Reducer<R>, accResult: R, tree: Package, path: string[], depth): R => {
+const reduce = <R>(reducer: Reducer<R>, accResult: R, tree: Package, path: string[], depth: number): R => {
   if (path.length > depth + 1) return accResult;
   return (tree.dependencies || [])
     .reduce<R>((acc, pkg) => reduce(reducer, acc, pkg, [ ...path, pkg.name ], depth), reducer(accResult, tree, path));
 };
 
-export default <R>(tree: Package, reducer: Reducer<R>, initialResult: R, depth = Infinity): R => {
+export default <R>(tree: Package, reducer: Reducer<R>, initialResult: R, depth: number = Infinity): R => {
   return reduce(reducer, initialResult, tree, [ tree.name ], depth);
 };
