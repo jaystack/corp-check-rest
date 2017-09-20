a6e088b9f5e07d3d36de47216382de9557fd895c
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-09-20 18:01:06 +0200

    hotfix

--- src/utils/reduceTree.ts
+++ src/utils/reduceTree.ts
@@ -5,11 +5,9 @@ export type Reducer<R> = {
 };
 
 const reduce = <R>(reducer: Reducer<R>, accResult: R, tree: Package, path: string[], depth): R => {
-  const rootValue = reducer(accResult, tree, path);
-  return path.length > depth + 1
-    ? rootValue
-    : (tree.dependencies || [])
-        .reduce<R>((acc, pkg) => reduce(reducer, acc, pkg, [ ...path, pkg.name ], depth), rootValue);
+  if (path.length > depth + 1) return accResult;
+  return (tree.dependencies || [])
+    .reduce<R>((acc, pkg) => reduce(reducer, acc, pkg, [ ...path, pkg.name ], depth), reducer(accResult, tree, path));
 };
 
 export default <R>(tree: Package, reducer: Reducer<R>, initialResult: R, depth = Infinity): R => {
