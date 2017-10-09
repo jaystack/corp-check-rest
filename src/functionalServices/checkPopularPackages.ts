6dc116d8742e318f01472614eebf105724fb1604
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-10-09 15:35:56 +0200

    add rest endpoit to CheckPopularDependencies

--- src/functionalServices/checkPopularPackages.ts
+++ src/functionalServices/checkPopularPackages.ts
@@ -1,14 +1,14 @@
-import { inject } from 'functionly';
+import { rest, inject } from 'functionly';
 import { CorpCheckRestService } from './corpCheckRestService';
 import { Validation } from './rest';
 import { popularPackageNames } from '../consts';
 
+@rest({ path: '/popular-packages', methods: [ 'post' ] })
 export class CheckPopularDependencies extends CorpCheckRestService {
   public async handle(@inject(Validation) validate) {
-    console.log(validate)
     for (const packageName of popularPackageNames) {
-      console.log(packageName)
-      await validate({ packageName });
+      console.log(packageName);
+      await validate.invoke({ packageName });
     }
   }
 }
